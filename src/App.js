c6d851f379ba6d10519ad8f888cfe48c3916e8de
Pranjal <pranjalgupta362@gmail.com>
2020-10-21 09:46:22 +0530

    Fixed some Bugs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+import  { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    <Navbar dark color="primary">
-      <div className="container">
-        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+    render(){
+    return (
+      <div className="App">
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu dishes={this.state.dishes} />
       </div>
-    </Navbar>
-    <Menu />
-    </div>
-  );
-}
-
+    );
+    }
+  }
 
 export default App;
