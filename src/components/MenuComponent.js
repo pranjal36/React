81999a5d1fc6887c04ca24231bf0b690c8747cb0
Pranjal <pranjalgupta362@gmail.com>
2020-10-21 22:20:07 +0530

    Added comments and card form to pics

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import DishetailComponent from "./DishdetailComponent.js";
+
 
 class Menu extends Component {
 
@@ -16,20 +18,8 @@ class Menu extends Component {
     }
 
     renderDish(dish) {
-        if (dish != null)
-            return(
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        else
-            return(
-                <div></div>
-            );
+        if (dish != null) return <DishetailComponent dish={dish} />;
+        else return <div />;
     }
 
     render() {
@@ -52,11 +42,7 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                <div className="row">
-                  <div  className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.state.selectedDish)}
-                  </div>
-                </div>
+                {this.renderDish(this.state.selectedDish)}
             </div>
         );
     }
